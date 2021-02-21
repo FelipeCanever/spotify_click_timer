fa4d3b53a10e459d0bc7fc0a2821d9cf4f825472
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-02-21 19:28:40 -0300

    Implement click logic

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from mouse import LeftButton
+from restartable_timer import RestartableTimer
+
+DURATION = 15
+
+def beep():
+	print("\a")
 
 if __name__ == "__main__":
 	button = LeftButton()
+	timer = RestartableTimer(DURATION, beep)
 
 	while True:
 		if button.released:
 			print("Left mouse button released.")
+			timer.restart()
