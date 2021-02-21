e7511091cdabb3b851827adbc005657d6e88bb22
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2021-02-21 16:04:05 -0300

    Create restartable timer

---
+++ restartable_timer.py
@@ -0,0 +1,23 @@
+from threading import Timer
+
+class RestartableTimer:
+	def __init__(self, duration, callback):
+		self.duration = duration
+		self.callback = callback
+
+		self._timer = None
+		self._reset()
+	
+	def _reset(self):
+		self._timer = Timer(self.duration, self.callback)
+	
+	def start(self):
+		self._timer.start()
+	
+	def cancel(self):
+		self._timer.cancel()
+
+	def restart(self):
+		self.cancel()
+		self._reset()
+		self.start()
